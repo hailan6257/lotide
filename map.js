8ab4c423199331a56b78f0cbe68985cf965ddf0d
Hailan Wang <hailan6257@email.com>
2021-11-25 20:57:01 -0500

    My version of map

---
+++ map.js
@@ -0,0 +1,33 @@
+// TEST IMPLEMENTATION
+const eqArrays = (array1, array2) => {
+  let aLength = array1.length, bLength = array2.length;
+  if (aLength !== bLength) {
+    return false;
+  }
+  for (let i = 0; i < aLength; i ++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+//ACTUAL FUNCTION
+const assertArraysEqual = (aArray, bArray) => {
+  console.log((eqArrays(aArray, bArray)) ? `✅Passed: ${aArray} === ${bArray}` : `🛑Failed: ${aArray} !== ${bArray}`);
+};
+
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+//TEST CODE
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
