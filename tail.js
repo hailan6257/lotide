27f48fe2f0c792fd703ceac1c237ad987b4d9229
Hailan Wang <hailan6257@email.com>
2021-11-29 13:52:56 -0500

    refeactored with export and require

--- tail.js
+++ tail.js
@@ -1,8 +1,4 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = (actual, expected) => {
-  console.log((actual === expected) ? `✅Passed: ${actual} === ${expected}` : `🛑Failed: ${actual} !== ${expected}`);
-};
-
 const tail = (array) => {
   let newArr =  [...array];
   if (newArr.length > 1) {
@@ -13,7 +9,4 @@ const tail = (array) => {
   }
 };
 
-//Test cases
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
