26a6f2b3e429ad5d7cd36fc2e740e473284712bf
Hailan Wang <hailan6257@email.com>
2021-11-29 15:51:05 -0500

    Add seperated test code

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -3,6 +3,3 @@ const assertArraysEqual = require('../assertArraysEqual');
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], ['1', 2, 3]);
 assertArraysEqual([1, 2, 3], [4, 2, 3]);
-/*
-
-*/
