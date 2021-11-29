40892bff0be6bdb4d9d0d253a5485bf6531439b2
Hailan Wang <hailan6257@email.com>
2021-11-29 15:46:03 -0500

    refeactored with export and require,add seperated test code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,8 @@
 // FUNCTION IMPLEMENTATION
-const eqArrays = (array1, array2) => {
-  let aLength = array1.length, bLength = array2.length;
-  if (aLength !== bLength) {
-    return false;
-  }
-  for (let i = 0; i < aLength; i ++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (aArray, bArray) => {
   console.log((eqArrays(aArray, bArray)) ? `✅Passed: ${aArray} === ${bArray}` : `🛑Failed: ${aArray} !== ${bArray}`);
 };
 
-
-// TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], ['1', 2, 3]);
-assertArraysEqual([1, 2, 3], [4, 2, 3]);
-/*
-
-*/
+module.exports = assertArraysEqual;
