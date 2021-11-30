49f62cb71676ba51d92f18cc8f5fad39946ad96a
Hailan Wang <hailan6257@email.com>
2021-11-29 19:31:10 -0500

    Add seperateed test code

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,17 @@
 //test code
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+const assert = require("chai").assert;
 const middle = require('../middle');
-assertArraysEqual([], middle([1]));
-assertArraysEqual([], middle([1, 2]));
-assertArraysEqual([2], middle([1, 2, 3]));
-assertArraysEqual([3], middle([1, 2, 3, 4, 5]));
-assertArraysEqual([2, 3], middle([1, 2, 3, 4]));
-assertArraysEqual([3, 4], middle([1, 2, 3, 4, 5, 6]));
+// assertArraysEqual([], middle([1]));
+// assertArraysEqual([], middle([1, 2]));
+// assertArraysEqual([2], middle([1, 2, 3]));
+// assertArraysEqual([3], middle([1, 2, 3, 4, 5]));
+// assertArraysEqual([2, 3], middle([1, 2, 3, 4]));
+// assertArraysEqual([3, 4], middle([1, 2, 3, 4, 5, 6]));
+describe('#midddle', function() {
+  it("should return [] for [1]", function() {
+    const mid = middle([1]);
+    console.log(mid);
+    assert.deepEqual(mid, []);
+  });
+});
